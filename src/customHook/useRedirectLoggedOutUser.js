26acaba94f333257c021a8dedb38ef5a7ef6471b
BinoB <binob831@gmail.com>
2023-08-11 19:21:29 +0530

    change

--- src/customHook/useRedirectLoggedOutUser.js
+++ src/customHook/useRedirectLoggedOutUser.js
@@ -1,22 +1,20 @@
-import React, { useEffect } from "react";
-import { useDispatch ,useSelector} from "react-redux";
+/* import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { SET_LOGIN } from "../redux/features/auth/authSlice";
 import { getLoginStatus } from "../services/authService";
 import { toast } from "react-toastify";
-import { selectIsLoggedIn } from "../redux/features/auth/authSlice";
 
 const useRedirectLoggedOutUser = (path) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const isLoggedIn = useSelector(selectIsLoggedIn);
 
   useEffect(() => {
     const redirectLoggedOutUser = async () => {
       const isLoggedIn = await getLoginStatus();
       dispatch(SET_LOGIN(isLoggedIn));
 
-      if (isLoggedIn) {
+      if (!isLoggedIn) {
         toast.info("Session expired, please login to continue.");
         navigate(path);
         return;
@@ -26,4 +24,4 @@ const useRedirectLoggedOutUser = (path) => {
   }, [navigate, path, dispatch]);
 };
 
-export default useRedirectLoggedOutUser;
+export default useRedirectLoggedOutUser; */
